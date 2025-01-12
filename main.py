09f75ed9f5b71f10d7124c4803be90c6e5cd04f9
Rentoo <dreamYiZ@outlook.com>
2025-01-12 16:33:40 +0800

    -

--- main.py
+++ main.py
@@ -1,16 +1,64 @@
-# This is a sample Python script.
+# Importing libraries
+import os
+import cv2
+from PIL import Image
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Set path to the Google Drive folder with images
+path = "/content/drive/My Drive/Images"
+os.chdir(path)
+
+mean_height = 0
+mean_width = 0
+
+# Counting the number of images in the directory
+num_of_images = len([file for file in os.listdir('.') if file.endswith((".jpg", ".jpeg", ".png"))])
+print("Number of Images:", num_of_images)
+
+# Calculating the mean width and height of all images
+for file in os.listdir('.'):
+    if file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith("png"):
+        im = Image.open(os.path.join(path, file))
+        width, height = im.size
+        mean_width += width
+        mean_height += height
+
+# Averaging width and height
+mean_width = int(mean_width / num_of_images)
+mean_height = int(mean_height / num_of_images)
+
+# Resizing all images to the mean width and height
+for file in os.listdir('.'):
+    if file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith("png"):
+        im = Image.open(os.path.join(path, file))
+        # Use Image.LANCZOS instead of Image.ANTIALIAS for downsampling
+        im_resized = im.resize((mean_width, mean_height), Image.LANCZOS)
+        im_resized.save(file, 'JPEG', quality=95)
+        print(f"{file} is resized")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# Function to generate video
+def generate_video():
+    image_folder = path
+    video_name = 'mygeneratedvideo.avi'
 
+    images = [img for img in os.listdir(image_folder) if img.endswith((".jpg", ".jpeg", ".png"))]
+    print("Images:", images)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    # Set frame from the first image
+    frame = cv2.imread(os.path.join(image_folder, images[0]))
+    height, width, layers = frame.shape
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Video writer to create .avi file
+    video = cv2.VideoWriter(video_name, cv2.VideoWriter_fourcc(*'DIVX'), 1, (width, height))
+
+    # Appending images to video
+    for image in images:
+        video.write(cv2.imread(os.path.join(image_folder, image)))
+
+    # Release the video file
+    video.release()
+    cv2.destroyAllWindows()
+    print("Video generated successfully!")
+
+# Calling the function to generate the video
+generate_video()
